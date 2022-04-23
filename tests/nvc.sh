67f849cf2ceb057d698ea9205c31ab6e872b5022
hexinz <hexinz@umich.edu>
2022-04-23 18:01:41 +0000

    final version

---
+++ tests/nvc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+count=0
+
+# lower bound 100, upper bound 300
+make clean
+make
+while [ $count -lt 3000 ]
+do
+    low="$count"
+    up="`expr $count + 100`"
+    /home/vagrant/NVC/pin-2.14-71313-gcc.4.4.7-linux/intel64/bin/pinbin -p32 /home/vagrant/NVC/pin-2.14-71313-gcc.4.4.7-linux/ia32/bin/pinbin -t obj-intel64//dcache.so -_lb "$count" -_ub `expr $count + 100` -- /home/vagrant/NVC/Benchmarks/CrashTests/NPB3.3-OMP-C/bin/lh.A.x 2 200
+    count=`expr $count + 100`
+done
